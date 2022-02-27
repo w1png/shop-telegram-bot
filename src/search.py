325055ec440d5baeb3edd8be6a641b8759f78a6e
w1png <wonepng@gmail.com>
2022-02-27 16:07:48 +0300

    Improve search

--- src/search.py
+++ src/search.py
@@ -1,25 +1,53 @@
 import pymorphy2
+from pyparsing import opAssoc
 import item as itm
 
 m = pymorphy2.MorphAnalyzer()
 
-def get_normal_form(word):
-    return m.parse(word)[0].normal_form
+excluded_words = ["без", "в", "для", "до", "за", "из", "к", "под", "а", "о", "над", "на", "о", "об", "от", "перед", "по", "под", "при", "про", "с", "у"]
+
+def get_normal_forms(word):
+    return set(p.normal_form for p in m.parse(word))
     
 
-def pr(item):
-    print(item)
+class Query:
+    def __init__(self, results):
+        self.results = results
 
+    def __get_items(self):
+        return list(map(itm.Item, self.results.keys()))[::-1]
+
+    def match(self):
+        return self.__get_items()
+
+    def price(self):
+        return sorted(list(map(itm.Item, self.results.keys()))[::-1], key=lambda item: item.get_price())
+
+    def popular(self):
+        pass
+
+# a match in a name is 3 points
+# a match in a desc is 1 point
 def search_item(query):
-    probabilities = dict()
+    points = dict()
     for item in itm.get_item_list():
-        probabilities[item.get_id()] = 0
-        keywords = list(map(get_normal_form, item.get_name().split() + item.get_desc().split()))
-        print(keywords)
-        print(list(map(get_normal_form, query.split())))
-        for word in list(map(get_normal_form, query.split())):
-            probabilities[item.get_id()] += keywords.count(word)
-    return dict(filter(lambda item: item[1] != 0, dict(sorted(probabilities.items(), key=lambda item: item[1])).items()))
+        points[item.get_id()] = 0
+        for word in list(filter(lambda word: word not in excluded_words, query.split())):
+            for p in get_normal_forms(word):
+                for word_title in item.get_name().split():
+                    if p in get_normal_forms(word_title):
+                        points[item.get_id()] += 3
+                for word_title in item.get_desc().split():
+                    if p in get_normal_forms(word_title):
+                        points[item.get_id()] += 1
+        if points[item.get_id()] == 0:
+            points.pop(item.get_id())
+    print(points)
+    return Query(dict(sorted(points.items(), key=lambda item: item[1])))
 
 if __name__ == "__main__":
-    print(search_item("телефон с большим экраном"))
+    # Query()
+    res = search_item("телефон с большим экраном")
+    print(res.match())
+    print(res.price())
+
