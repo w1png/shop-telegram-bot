cf8af966ccc84b5991b713848188f3d1081d9ab3
w1png <wonepng@gmail.com>
2022-02-27 21:18:52 +0300

    Fix less chars for better mobile visibility

--- src/markups.py
+++ src/markups.py
@@ -114,7 +114,7 @@ def get_markup_cart(user):
     markup = types.InlineKeyboardMarkup()
     delivery_price = '{:.2f}'.format(float(settings.get_delivery_price()))
     for item_and_amount in user.get_cart_amount():
-        markup.add(types.InlineKeyboardButton(text=f"{item_and_amount[0].get_name()[:100-len(f' - {item_and_amount[1]}шт.')-3]}... - {item_and_amount[1]}шт.", callback_data=f"viewItem{item_and_amount[0].get_id()}"))
+        markup.add(types.InlineKeyboardButton(text=f"{item_and_amount[0].get_name()[:50-len(f' - {item_and_amount[1]}шт.')-3]}... - {item_and_amount[1]}шт.", callback_data=f"viewItem{item_and_amount[0].get_id()}"))
         markup.add(types.InlineKeyboardButton(text=f"{item_and_amount[0].get_price() * item_and_amount[1]}руб.", callback_data="None"), types.InlineKeyboardButton(text=tt.plus, callback_data=f"addToCartFromCart{item_and_amount[0].get_id()}"), types.InlineKeyboardButton(text=tt.minus, callback_data=f"removeFromCartFromCart{item_and_amount[0].get_id()}"))
     if settings.is_delivery_enabled():
         markup.add(types.InlineKeyboardButton(text=tt.delivery_on(delivery_price) if user.is_cart_delivery() else tt.delivery_off(delivery_price), callback_data="changeCartDelivery"))
