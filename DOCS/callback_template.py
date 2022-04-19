66ed39adc7e0619f346b3b95c1a8762ce6bf0143
w1png <wonepng@gmail.com>
2022-04-19 14:17:37 +0300

    Add callback_template

--- DOCS/callback_template.py
+++ DOCS/callback_template.py
@@ -1,7 +1,7 @@
 from aiogram import Bot
 from users import User
 
-async def execute(bot: Bot, user: User, message_id: int):
+async def execute(bot: Bot, user: User, message_id: int, data: dict):
    await bot.edit_message_text(
        chat_id=user.id,
        message_id=message_id,
