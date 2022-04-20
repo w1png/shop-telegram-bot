7ce875959e7db6a1adf34584658a8c01d8cc2980
w1png <wonepng@gmail.com>
2022-04-20 04:45:12 +0300

    Fix spacing

--- DOCS/callback_template.py
+++ DOCS/callback_template.py
@@ -2,9 +2,9 @@ from aiogram import Bot
 from users import User
 
 async def execute(bot: Bot, user: User, message_id: int, data: dict):
-   await bot.edit_message_text(
+    await bot.edit_message_text(
        chat_id=user.id,
        message_id=message_id,
        text="Text",
-   ) 
+    ) 
 
