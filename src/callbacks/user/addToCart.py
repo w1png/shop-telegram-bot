5511d39dc5f9c1175ac552c4a1a8eab704f4c27e
w1png <wonepng@gmail.com>
2022-04-21 17:40:18 +0300

    Add addToCart and edit item

---
+++ src/callbacks/user/addToCart.py
@@ -0,0 +1,12 @@
+from aiogram import Bot
+from importlib import import_module
+
+from users import User
+from items import Item
+
+async def execute(bot: Bot, user: User, message_id: int, data: dict):
+    item = Item(data["item_id"])
+    user.cart.items.add(item)
+    
+    await import_module("callbacks.user.item").execute(bot, user, message_id, data)
+
--- src/callbacks/user/item.py
+++ src/callbacks/user/item.py
@@ -2,17 +2,30 @@ from aiogram import Bot
 
 from users import User
 from items import Item
-from constants import language
+from constants import language, JSON_USER
 from markups import markups
 
 async def execute(bot: Bot, user: User, message_id: int, data: dict):
     item = Item(data["item_id"])
-    text = language.item(item) + (f"\n\nВ корзине: {user.cart.items.dict[item.id]} шт." if item in user.cart.items else "")
+    text = language.item(item)
+    item_cart_quantity = user.cart.items.dict[item.id]
     
+    add_to_cart_callback = f'{{"role": "user", "item_id": {item.id}}}addToCart'
+    cart_button = (language.add_to_cart, add_to_cart_callback) if item.amount != 0 else (language.not_in_stock, f'{JSON_USER}None')
+    if item in user.cart.items:
+        text += f"\n\nВ корзине: {user.cart.items.dict[item.id]} шт."
+        cart_button = ((
+            (language.minus, f'{{"role": "user", "item_id": {item.id}}}removeFromCart'),
+            (item_cart_quantity, f'{JSON_USER}None'),
+            (language.plus, add_to_cart_callback) if item_cart_quantity <= item.amount else (language.cross, f'{JSON_USER}None')
+        ))
+
+    btn_back = (language.back, f'{{"role": "user", "category_id": {item.category_id}}}category')
     markup = markups.create([
-        (language.add_to_cart, f'{{"role": "user", "item_id": {item.id}}}addToCart') if item.amount > 0 else (language.not_in_stock, 'None'),
-        (language.back, f'{{"role": "user", "category_id": {item.category_id}}}category')
+        cart_button,
+        btn_back
     ])
+
 
     if not item.image.is_hidden and item.image.filename:
         return await bot.send_photo(
