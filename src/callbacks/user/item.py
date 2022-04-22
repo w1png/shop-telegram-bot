2911469164b3b40ef2434b909a52a643f68ec260
w1png <wonepng@gmail.com>
2022-04-22 08:22:49 +0300

    Fix item amount 0 butotns

--- src/callbacks/user/item.py
+++ src/callbacks/user/item.py
@@ -8,16 +8,17 @@ from markups import markups
 async def execute(bot: Bot, user: User, message_id: int, data: dict):
     item = Item(data["item_id"])
     text = language.item(item)
-    item_cart_quantity = user.cart.items.dict[item.id]
     
     add_to_cart_callback = f'{{"role": "user", "item_id": {item.id}}}addToCart'
+    cross_button = (language.cross, f'{JSON_USER}None')
     cart_button = (language.add_to_cart, add_to_cart_callback) if item.amount != 0 else (language.not_in_stock, f'{JSON_USER}None')
     if item in user.cart.items:
+        item_cart_quantity = user.cart.items.dict[item.id]
         text += f"\n\nВ корзине: {user.cart.items.dict[item.id]} шт."
-        cart_button = ((
-            (language.minus, f'{{"role": "user", "item_id": {item.id}}}removeFromCart'),
+        cart_button  = ((
+            (language.minus, f'{{"role": "user", "item_id": {item.id}}}removeFromCart') if item_cart_quantity != 0 else cross_button,
             (item_cart_quantity, f'{JSON_USER}None'),
-            (language.plus, add_to_cart_callback) if item_cart_quantity <= item.amount else (language.cross, f'{JSON_USER}None')
+            (language.plus, add_to_cart_callback) if item_cart_quantity < item.amount else cross_button 
         ))
 
     btn_back = (language.back, f'{{"role": "user", "category_id": {item.category_id}}}category')
