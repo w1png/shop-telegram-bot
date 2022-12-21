6403e3b7e303e4afac10aeeafbd6f139483cc5ae
Denis Smirnov <w1png@yandex.ru>
2022-12-21 03:53:44 +0300

    Add contacts

---
+++ src/callbacks/user/contacts.py
@@ -0,0 +1,16 @@
+from aiogram import types
+import models
+import constants
+from markups import markups
+
+
+async def execute(callback_query: types.CallbackQuery, user: models.users.User, data: dict, message=None) -> None:
+    text = constants.config["info"]["contacts"]
+    markup = markups.create([
+        (constants.language.back, f'{constants.JSON_USER}faq'),
+    ])
+
+    if message:
+        return await message.answer(text, reply_markup=markup)
+    await callback_query.message.edit_text(text, reply_markup=markup)
+
