adaa44ddf4938b19a8e42a432f1e310ecf787cbb
Denis Smirnov <w1png@yandex.ru>
2022-12-21 09:01:47 +0300

    Add catalogue

---
+++ src/callbacks/user/catalogue.py
@@ -0,0 +1,21 @@
+from aiogram import types
+import models
+import constants
+from markups import markups
+import models
+import asyncio
+
+
+async def execute(callback_query: types.CallbackQuery, user: models.users.User, data: dict, message=None) -> None:
+    text = constants.language.catalogue
+
+    markup = markups.create([
+        (await category.name, f'{{"r":"user","cid":{category.id}}}category')
+        for category in await models.categories.get_categories()
+    ])
+
+    if message:
+        return await message.answer(text, reply_markup=markup)
+    await callback_query.message.edit_text(text, reply_markup=markup)
+
+
