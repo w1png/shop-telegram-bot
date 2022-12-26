e1947a59b3b8a0488608f52da6bc6c5b8cfaa09c
Denis Smirnov <w1png@yandex.ru>
2022-12-26 12:28:55 +0300

    Add settings

---
+++ src/callbacks/admin/settings.py
@@ -0,0 +1,19 @@
+from aiogram import types
+import models
+import constants
+from markups import markups
+
+
+async def execute(callback_query: types.CallbackQuery, user: models.users.User, data: dict, message=None) -> None:
+     await callback_query.message.edit_text(
+        text=constants.language.settings,
+        reply_markup=markups.create([
+            (constants.language.main_settings, f"{constants.JSON_ADMIN}main_settings"),
+            (constants.language.item_settings, f"{constants.JSON_ADMIN}item_settings"),
+            (constants.language.system_settings, f"{constants.JSON_ADMIN}system_settings"),
+            (constants.language.stats_settings, f"{constants.JSON_ADMIN}stats_settings"),
+            (constants.language.back, f"{constants.JSON_ADMIN}adminPanel"),
+        ])
+    )
+
+
