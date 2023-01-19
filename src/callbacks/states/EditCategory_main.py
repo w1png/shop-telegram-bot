4b266b2135a99965aa7bc673b194d20bbe3e8539
Denis Smirnov <w1png@yandex.ru>
2023-01-19 03:57:22 +0300

    Use match case in EditCategory_main

--- src/callbacks/states/EditCategory_main.py
+++ src/callbacks/states/EditCategory_main.py
@@ -19,28 +19,29 @@ async def execute(callback_query: types.CallbackQuery, user: models.users.User, 
     text = constants.language.unknown_error
 
     markup = []
-    if call == "editCategoryName":
-        await states.EditCategory.name.set()
-        text = constants.language.input_category_name
-    elif call == "editCategoryPC":
-        await states.EditCategory.parent_category.set()
-        text = constants.language.set_parent_category
-        markup = [
-            (await category.name, f'{{"r":"admin","pid":{category.id}}}setCategoryPC')
-            for category in filter(lambda category: category.id != state_data["category_id"], await models.categories.get_categories())
-        ]
-        markup.append((constants.language.skip, f'{{"r":"admin","pid":0}}setCategoryPC'))
-    elif call == "deleteCategory":
-        await states.EditCategory.delete.set()
-        text = constants.language.confirm_delete_category
-        markup = [(
-            (constants.language.yes, f'{{"r":"admin"}}deleteCategory'),
-            (constants.language.no, f'{{"r":"admin","d":"editCategory","cid":{category.id}}}cancel')
-        )]
-    elif call == "toggleHideCategory":
-        await category.set_is_hidden(not await category.is_hidden)
-        await importlib.import_module("callbacks.admin.editCategory").execute(callback_query, user, data)
-        return
+    match call:
+        case "editCategoryName":
+            await states.EditCategory.name.set()
+            text = constants.language.input_category_name
+        case "editCategoryPC":
+            await states.EditCategory.parent_category.set()
+            text = constants.language.set_parent_category
+            markup = [
+                (await category.name, f'{{"r":"admin","pid":{category.id}}}setCategoryPC')
+                for category in filter(lambda category: category.id != state_data["category_id"], await models.categories.get_categories())
+            ]
+            markup.append((constants.language.skip, f'{{"r":"admin","pid":0}}setCategoryPC'))
+        case "deleteCategory":
+            await states.EditCategory.delete.set()
+            text = constants.language.confirm_delete_category
+            markup = [(
+                (constants.language.yes, f'{{"r":"admin"}}deleteCategory'),
+                (constants.language.no, f'{{"r":"admin","d":"editCategory","cid":{category.id}}}cancel')
+            )]
+        case "toggleHideCategory":
+            await category.set_is_hidden(not await category.is_hidden)
+            await importlib.import_module("callbacks.admin.editCategory").execute(callback_query, user, data)
+            return
     if call != "deleteCategory":
         markup.append((constants.language.back, f'{{"r":"admin","d":"editCategory","cid":{category.id}}}cancel'))
 
